7026ec175ffa5228bb6ffed579288c515719c84b
祝望舒 <freewaterzws@hotmail.com>
2015-07-21 21:15:22 +0800

    Initial Commit

---
+++ Booshit product/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Booshit product
+//
+//  Created by Wangshu Zhu on 7/21/15.
+//  Copyright © 2015 Wangshu Zhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
